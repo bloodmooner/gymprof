3d9b43cd11de5cd38f263dd8a8af8bdba0bd2f1f
Sergey Goncharuk <fleshed914@gmail.com>
2022-07-07 21:27:33 +0300

    feat: add second slider to product

--- js/script.js
+++ js/script.js
@@ -5,9 +5,51 @@ $(document).ready(function(){
         'vertical': true,
         'slidesToShow': 3,
         'arrows': true,
-        nextArrow: $(document).find('.product-slider__arrow_bottom'),
-        prevArrow: $(document).find('.product-slider__arrow_top'),
-      });
+        nextArrow: $(document).find('.product-slider__arrow_top'),
+        prevArrow: $(document).find('.product-slider__arrow_bottom'),
+        asNavFor: '.product__image-slider',
+    });
+
+
+    $('.product__image-slider').slick({
+        'slidesToShow': 1,
+        'arrows': false,
+        fade: true,
+        cssEase: 'linear',
+    });
   
+
+
+    let videoPlayBtn = $('.video__play-btn');
+    let videoBg = $('.video__background');
+    let videoYoutube = $('.video__youtube');
+  
+    videoPlayBtn.on('click', function() {
+      let videoSrc = videoYoutube.prop('src');
+  
+      videoYoutube.attr('src', `${videoSrc}&autoplay=1`);
+      videoBg.css('display', 'none');
+      videoPlayBtn.css('display', 'none');
+  
+      $(".youtube").each(function() {
+        // Based on the YouTube ID, we can easily find the thumbnail image
+        
+  
+        // Overlay the Play icon to make it look like a video player
+        $(this).append($('<div/>', {'class': 'play'}));
+  
+        
+            // Create an iFrame with autoplay set to true
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+  
+            // The height and width of the iFrame should be the same as parent
+            var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': '100%', 'height': '100%' })
+  
+            // Replace the YouTube thumbnail with YouTube HTML5 Player
+            $(this).replaceWith(iframe);
+        
+    });
+  });
 });
 
