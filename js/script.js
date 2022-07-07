a2d77dcd8c2e17d1c2333504a474967ec150c26a
Sergey Goncharuk <fleshed914@gmail.com>
2022-07-07 15:45:16 +0300

    feat: add vertical slider to product page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
 
+    $('.product-slider__wrapper').slick({
+        'vertical': true,
+        'slidesToShow': 3,
+        'arrows': true,
+        nextArrow: $(document).find('.product-slider__arrow_bottom'),
+        prevArrow: $(document).find('.product-slider__arrow_top'),
+      });
   
 });
 
